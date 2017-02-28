60a6820173a26770e539bc1aabe52315a92c3b72
“Tatu <koivisto.tatu@gmail.com>
2017-02-28 18:41:02 +0200

    JOB DONE

--- js/story-slides.js
+++ js/story-slides.js
@@ -4,6 +4,7 @@ var requestURL = "https://koivist9.firebaseio.com/.json"
 var position = 1;
 
 
+
 /* SLIDE TEXT CHANGE */
 var slideContents = $.getJSON(requestURL, function (data) {
 
@@ -32,11 +33,14 @@ var slideContents = $.getJSON(requestURL, function (data) {
 );
 
 var slideAmount = $('.story-comtainer').length;
+var endPosition = (slideAmount - 1) * -100;
+console.log(endPosition); 
 console.log(slideAmount);
 var text11 = $(".text-1-1");
 var text21 = $(".text-2-1");
 var text31 = $(".text-3-1");
 var text41 = $(".text-4-1");
+var storyContainer = $(".story-comtainer");
 
 var textAnimation1 = function(){
     TweenMax.to(text11, 0.3, { y: "-=10",autoAlpha:"1", delay:0.5});
@@ -59,14 +63,34 @@ var setOthersToDefault = function(){
     TweenMax.to(text41, 0.1, { y: "0",autoAlpha:"0"});
 }
 
+var playStatus = "pause";
+var myInterval = setInterval(nextSlide, 4000);
 
+var playPause = function(){
+    if(playStatus === "play"){
+        $(".play-button").html("PLAY");
+        playStatus = "pause";
+        clearInterval(myInterval);
+        console.log("pause pressed")
+    }else {
+        $(".play-button").html("PAUSE");
+        playStatus = "play";
+        myInterval = setInterval(nextSlide, 4000);
+        console.log("play pressed")
+    }
+}
 
 /* Back Slide */
 var backSlide = function(){
-    if(position > 1){
-        $(".story-comtainer").velocity({translateY:'+=100%'}, {duration:300});
+    if(position == 1){
+        position = 4;
+        storyContainer.velocity({translateY:endPosition + "%"});
+        textAnimation4();  
+    }else if(position > 1){
+        storyContainer.velocity({translateY:'+=100%'}, {duration:300});
         position -= 1;
         slideAction();
+        console.log(position);
     }else{
         console.log("mene eteenpäin");
     }
@@ -75,18 +99,26 @@ var backSlide = function(){
 
 /* Next Slide */
 var nextSlide = function(){
-    if(position < slideAmount){
-    $(".story-comtainer").velocity({translateY:'-=100%'}, {duration:300});
+    if(position === slideAmount){
+        position = 1;
+        textAnimation1();
+        storyContainer.velocity({translateY:'0%'});
+    }else if(position < slideAmount){
+    storyContainer.velocity({translateY:'-=100%'}, {duration:300});
     position += 1;
     slideAction();
+    console.log(position);
   }else {
     console.log("mene taaksepäin");
+
   }
 }
 
-
+  $(".play-button").click(playPause)
   $(".next-button").click(nextSlide);
   $(".back-button").click(backSlide);
+
+
 
 var slideAction = function(){
     if(position == 1) {
@@ -103,8 +135,6 @@ var slideAction = function(){
         setOthersToDefault();
     }
 }
-
-
 
 
 });
