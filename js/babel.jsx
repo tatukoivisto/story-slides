f21d54e0ce5c40649f534c8319e5245b1d5d7b98
“Tatu <koivisto.tatu@gmail.com>
2017-02-28 15:57:24 +0200

    fix

---
+++ js/babel.jsx
@@ -0,0 +1,37 @@
+/*var Greeting = React.createClass({
+  render: function() {
+    return (
+ 
+      <div className="story-comtainer">
+      <div className="main-text-container">
+      <h1 className="main-text">TEKSTIT LATAUTUVAT
+      </h1>
+      </div>
+      <div className="bg-image"></div>
+
+      </div>
+      )
+  }
+}
+);
+
+ReactDOM.render(
+  <Greeting/>,
+  document.getElementById('slide1')
+  );
+
+
+*/
+
+
+/* BACKGROUND IMAGE ANIMATION 
+function imageAnimation() { 
+    $(".bg-image").velocity({scale: 1.2}, {duration:10000,easing:'swing',loop:true}).velocity("reverse");
+}
+imageAnimation();
+*/
+
+
+
+
+
--- js/story-slides.js
+++ js/story-slides.js
@@ -1,44 +1,110 @@
+$(document).ready(function(){
 
 var requestURL = "https://koivist9.firebaseio.com/.json"
+var position = 1;
 
-/* SLIDE TEKSTIN VAIHTAMINEN */
+
+/* SLIDE TEXT CHANGE */
 var slideContents = $.getJSON(requestURL, function (data) {
 
     	var mainJSON = data;	
     	console.log(mainJSON);
     
     	function changeOne() {
-    		$(".main-text").html(mainJSON.slide1.content);
+    		$(".text-1-1").html(mainJSON.slide1.content);
     	}
-    	changeOne();
-
+        changeOne();
     	function changeTwo() {
-    		$(".main-text").html(mainJSON.slide2.content);
+    		$(".text-2-1").html(mainJSON.slide2.content);
     	}
-    	setTimeout(changeTwo, 4000);
-
+        changeTwo();
     	function changeThree() {
-    		$(".main-text").html(mainJSON.slide3.content);
+    		$(".text-3-1").html(mainJSON.slide3.content);
     	}
-    	setTimeout(changeThree, 8000);
+        changeThree();
+        function changeFour() {
+            $(".text-4-1").html(mainJSON.slide4.content);
+        }
+        changeFour();
+
 
     }
 );
 
-$(".bg-image").velocity({scale: 1.2}, {duration:10000,easing:'swing',loop:true})
+var slideAmount = $('.story-comtainer').length;
+console.log(slideAmount);
+var text11 = $(".text-1-1");
+var text21 = $(".text-2-1");
+var text31 = $(".text-3-1");
+var text41 = $(".text-4-1");
 
-.velocity("reverse");
+var textAnimation1 = function(){
+    TweenMax.to(text11, 0.3, { y: "-=10",autoAlpha:"1", delay:0.5});
+}
+textAnimation1();
+var textAnimation2 = function(){
+    TweenMax.to(text21, 0.3, { y: "-=10",autoAlpha:"1", delay:0.5});
+}
+var textAnimation3 = function(){
+    TweenMax.to(text31, 0.3, { y: "-=10",autoAlpha:"1", delay:0.5});
+}
+var textAnimation4 = function(){
+    TweenMax.to(text41, 0.3, { y: "-=10",autoAlpha:"1", delay:0.5});
+}
+
+var setOthersToDefault = function(){
+    TweenMax.to(text11, 0.1, { y: "0",autoAlpha:"0"});
+    TweenMax.to(text21, 0.1, { y: "0",autoAlpha:"0"});
+    TweenMax.to(text31, 0.1, { y: "0",autoAlpha:"0"});
+    TweenMax.to(text41, 0.1, { y: "0",autoAlpha:"0"});
+}
+
+
+
+/* Back Slide */
+var backSlide = function(){
+    if(position > 1){
+        $(".story-comtainer").velocity({translateY:'+=100%'}, {duration:300});
+        position -= 1;
+        slideAction();
+    }else{
+        console.log("mene eteenpäin");
+    }
+}
+
+
+/* Next Slide */
+var nextSlide = function(){
+    if(position < slideAmount){
+    $(".story-comtainer").velocity({translateY:'-=100%'}, {duration:300});
+    position += 1;
+    slideAction();
+  }else {
+    console.log("mene taaksepäin");
+  }
+}
+
+
+  $(".next-button").click(nextSlide);
+  $(".back-button").click(backSlide);
+
+var slideAction = function(){
+    if(position == 1) {
+        textAnimation1();
+        setOthersToDefault();
+    }else if(position == 2){
+        textAnimation2();
+        setOthersToDefault();
+    }else if(position == 3){
+        textAnimation3();    
+        setOthersToDefault();
+    }else if(position == 4){
+        textAnimation4();  
+        setOthersToDefault();
+    }
+}
 
 
 
 
-
-/*
-$(document).ready(function(){
-    $("button").click(function(){
-        $.getJSON("https://koivist9.firebaseio.com/.json", function(result){
-            console.log(result);
-        });
-    });
-});*/
-
+});
